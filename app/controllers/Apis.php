107e2eb3a21b754e5426afcb8c99542a71aa054d
Developer <a6e6s1@gmail.com>
2020-04-16 04:33:19 +0300

    Donation API

--- app/controllers/Apis.php
+++ app/controllers/Apis.php
@@ -18,19 +18,22 @@ class Apis extends Controller
         ];
         echo json_encode($data);
     }
-/**
- * donation API 
- *
- * @return json
- */
+    /**
+     * donation API 
+     *
+     * @return json
+     */
     public function donations()
     {
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
         if (isset($_POST['api_key']) && isset($_POST['api_user'])) { // check if credential is sent
             $api_settings = json_decode($this->apiModel->getSettings('api')->value); // load API settings
             if ($api_settings->api_enable) {
                 //validate credential
                 if ($api_settings->api_key == $_POST['api_key'] && $api_settings->api_user == $_POST['api_user']) {
-                    $donations = $this->apiModel->getDonations();
+                    isset($_POST['start']) ? $start = (int) $_POST['start'] : $start = 0;
+                    isset($_POST['count']) ? $count = (int) $_POST['count'] : $count = 100;
+                    $donations = $this->apiModel->getDonations($start, $count);
                     $data = [
                         'status' => 'success',
                         'code' => 100,
--- app/models/Api.php
+++ app/models/Api.php
@@ -22,13 +22,13 @@ class Api extends Model
     }
 
 
-    public function getDonations($start = 1, $count = 100)
+    public function getDonations($start = 0, $count = 100)
     {
         return $this->queryResult(
-            'SELECT CONCAT("' . MEDIAURL . '/../files/banktransfer/", `banktransferproof`) as proof, donations.donation_id, donations.donation_identifier,
+            'SELECT donations.donation_id, donations.donation_identifier, CONCAT("' . MEDIAURL . '/../files/banktransfer/", `banktransferproof`) as proof,
          donors.full_name as donor, projects.project_number as AX_ItemID, donations.amount, donations.quantity, donations.total, donations.project_id, donations.donor_id, 
          donations.status,from_unixtime(donations.create_date) as date FROM donations, donors, projects
-         WHERE projects.project_id = donations.project_id AND donations.donor_id = donors.donor_id ORDER BY donations.create_date LIMIT ' . $start .' , '. $count 
+         WHERE donations.status <> 2 AND projects.project_id = donations.project_id AND donations.donor_id = donors.donor_id ORDER BY donations.create_date LIMIT ' . $start .' , '. $count 
         );
     }
 }

1c62fe65dc2d0312c7778c494826840e700b904e
unknown <a6e6s1@gmail.com>
2020-05-17 04:58:13 +0300

    small fixis with file manager and https force

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -1,3 +1,8 @@
+<?php
+// if (!$_SERVER['HTTPS']) {
+//     header("Location: https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+// }
+?>
 <!DOCTYPE html>
 <html lang="ar" dir="rtl">
 <!--

088b8a6577cb827c2257a4604ca9cb4874e457e1
unknown <a6e6s1@gmail.com>
2020-06-15 23:01:10 +0300

    working on API adding order ,donation request

--- app/controllers/Apis.php
+++ app/controllers/Apis.php
@@ -32,8 +32,53 @@ class Apis extends Controller
                 //validate credential
                 if ($auth['authorized']) {
                     isset($_POST['start']) ? $start = (int) $_POST['start'] : $start = 0;
-                    isset($_POST['count']) ? $count = (int) $_POST['count'] : $count = 100;
+                    isset($_POST['count']) ? $count = (int) $_POST['count'] : $count = 20;
                     $donations = $this->apiModel->getDonations($start, $count);
+                    $data = [
+                        'status' => 'success',
+                        'code' => 100,
+                        'msg' => 'Successfully connected',
+                        'donations' => $donations,
+                    ];
+                } else { // wrong user or key
+                    $data = [
+                        'status' => 'error',
+                        'code' => 102,
+                        'msg' => 'Wrong Credential check user and API key',
+                    ];
+                }
+            } else { // API not enabled
+                $data = [
+                    'status' => 'error',
+                    'code' => 103,
+                    'msg' => 'API not enabled',
+                ];
+            }
+        } else { // no credential
+            $data = [
+                'status' => 'error',
+                'code' => 101,
+                'msg' => 'Invalid Credential',
+            ];
+        }
+        echo json_encode($data);
+    }
+        /**
+     * donation API 
+     *
+     * @return json
+     */
+    public function orders()
+    {
+        $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+        if (isset($_POST['api_key']) && isset($_POST['api_user'])) { // check if credential is sent
+            $auth = $this->apiModel->auth($_POST['api_user'], $_POST['api_key']); // load API settings
+            if ($auth['enable']) {
+                //validate credential
+                if ($auth['authorized']) {
+                    isset($_POST['start']) ? $start = (int) $_POST['start'] : $start = 0;
+                    isset($_POST['count']) ? $count = (int) $_POST['count'] : $count = 20;
+                    $donations = $this->apiModel->getOrders($start, $count);
                     $data = [
                         'status' => 'success',
                         'code' => 100,
--- app/models/Api.php
+++ app/models/Api.php
@@ -28,22 +28,42 @@ class Api extends Model
      * @param integer $count
      * @return object
      */
-    public function getDonations($start = 0, $count = 100)
+    public function getDonations($start = 0, $count = 20)
     {
-        // return $this->queryResult(
-        //     'SELECT donations.donation_id, donations.donation_identifier, CONCAT("' . MEDIAURL . '/../files/banktransfer/", `banktransferproof`) as proof,
-        //  donors.full_name as donor, projects.project_number as AX_ItemID, donations.amount, donations.quantity, donations.total, donations.project_id, donations.donor_id, 
-        //  donations.status,from_unixtime(donations.create_date) as date FROM donations, donors, projects
-        //  WHERE donations.status <> 2 AND projects.project_id = donations.project_id AND donations.donor_id = donors.donor_id ORDER BY donations.create_date LIMIT ' . $start . ' , ' . $count
-        // );
         return $this->queryResult(
-            'SELECT donations.*,orders.order_identifier as `order`, projects.name as project FROM donations  ,projects, orders, donors
-            WHERE donations.status <> 2 AND projects.project_id = donations.project_id AND orders.donor_id = donors.donor_id 
-            ORDER BY donations.create_date LIMIT ' . $start . ' , ' . $count
+            'SELECT donations.*,orders.order_identifier as `order`, projects.name as project,
+             from_unixtime(donations.create_date) as create_date, from_unixtime(donations.modified_date) as modified_date 
+             FROM donations  ,projects, orders, donors
+             WHERE donations.status <> 2 AND projects.project_id = donations.project_id AND orders.donor_id = donors.donor_id 
+             ORDER BY donations.create_date LIMIT ' . $start . ' , ' . $count
         );
     }
 
-
+    /**
+     * get all orders
+     *
+     * @param integer $start
+     * @param integer $count
+     * @return object
+     */
+    public function getOrders($start = 0, $count = 20)
+    {
+        return $this->queryResult(
+            'SELECT ord.*, CONCAT("' . MEDIAURL . '/../files/banktransfer/", `banktransferproof`) as banktransferproof,
+             payment_methods.title as payment_method, donors.full_name as donor, donors.mobile,
+             from_unixtime(ord.create_date) as create_date, from_unixtime(ord.modified_date) as modified_date
+             FROM orders ord , donors,payment_methods 
+             WHERE ord.status <> 2 AND donors.donor_id = ord.donor_id AND ord.payment_method_id = payment_methods.payment_id
+             ORDER BY ord.create_date LIMIT ' . $start . ' , ' . $count
+        );
+    }
+    /**
+     * check user API authintcation 
+     *
+     * @param [string] $user
+     * @param [string] $key
+     * @return array
+     */
     public function auth($user, $key)
     {
         $api_settings = json_decode($this->getSettings('api')->value); // load API settings

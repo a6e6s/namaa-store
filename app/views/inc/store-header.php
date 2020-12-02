17d183852fd31cd8f052c1350219ac2051adfec6
Ahmed Elmahdy <a6e6s1@gmail.com>
2020-12-02 15:35:40 +0300

    employee store profile

---
+++ app/views/inc/store-header.php
@@ -0,0 +1,97 @@
+<?php
+// if (!$_SERVER['HTTPS']) {
+//     header("Location: https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+// }
+?>
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+<!--
+Copyright (C) 2020 Easy CMS Framework Ahmed Elmahdy
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License
+@license    https://opensource.org/licenses/GPL-3.0
+
+@package    Easy CMS MVC framework
+@author     Ahmed Elmahdy
+@link       https://ahmedx.com
+
+For more information about the author , see <http://www.ahmedx.com/>.
+-->
+
+<head>
+    <!-- Primary Meta Tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <meta name="keywords" content="<?php echo $this->meta->keywords; ?>">
+    <meta name="title" content="<?php echo $this->meta->title; ?>">
+    <meta name="description" content="<?php echo $this->meta->description; ?>">
+    <meta name="author" content="Ahmed Elmahdy">
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="<?php echo "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" ?>">
+    <meta property="og:title" content="<?php echo $this->meta->title; ?>">
+    <meta property="og:description" content="<?php echo $this->meta->description; ?>">
+    <meta property="og:image" content="<?php echo $this->meta->image; ?>">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="<?php echo "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]" ?>">
+    <meta property="twitter:title" content="<?php echo $this->meta->title; ?>">
+    <meta property="twitter:description" content="<?php echo $this->meta->description; ?>">
+    <meta property="twitter:image" content="<?php echo $this->meta->image; ?>">
+
+    <?php echo $this->meta->headerCode; ?>
+    <link rel="shortcut icon" href="<?php echo URLROOT; ?>/templates/default/images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="<?php echo URLROOT; ?>/templates/default/images/favicon.ico" type="image/x-icon">
+    <title><?php echo ($data['pageTitle']) ?? SITENAME; ?></title>
+    <!--- Bootstrap 4  --->
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/templates/default/css/bootstrap.min.css">
+    <!--- Icofont file --->
+    <link rel="stylesheet" type="text/css" href="<?php echo URLROOT; ?>/templates/default/css/icofont.min.css">
+
+    <!-- Owl Stylesheets -->
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/templates/default/vendors/owlcarousel/assets/owl.carousel.min.css">
+    <!--- Google fonts -->
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/templates/default/css/animate.css">
+    <link href="https://fonts.googleapis.com/css?family=Cairo&display=swap" rel="stylesheet">
+
+    <!--- CSS style --->
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/templates/default/css/style.css">
+    <?php echo isset($data['site_settings']->header_code) ? $data['site_settings']->header_code : ''; ?>
+</head>
+
+<body style="background:<?php echo $this->meta->background; ?>;">
+    <div class="preloader text-center">
+        <div class="text-center">
+            <img src="<?php echo URLROOT; ?>/templates/default/images/icon.gif" alt="">
+        </div>
+    </div>
+    <div class="container">
+        <section class="row py-2" id="top-bar">
+            <div class="col-5 col-md-7">
+                <a href="<?php echo isset($_SESSION['store']) ? URLROOT . '/store/' . $_SESSION['store']['alias'] : URLROOT; ?>" class="logo float-right">
+                    <img src="<?php echo empty($data['site_settings']->logo) ? '/' : MEDIAURL . '/' . $data['site_settings']->logo; ?>" height="60" alt="Namaa logo" class="img-fluid">
+                </a>
+            </div>
+            <div class="col-5 col-md-4 pt-3">
+                <div class="user float-left">
+                    <div class="nav">
+                        <li class="nav-item">
+                            <a title="Profile" class="nav-link text-dark border-left border-dark" href="<?php echo URLROOT . "/donors/login"; ?>"><i class="icofont-user"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-dark" href="<?php echo URLROOT . "/carts"; ?>">
+                                <span class="cart-num d-sm-block d-lg-none"><?php echo isset($_SESSION['cart']) ? $_SESSION['cart']['totalQty'] : ''; ?></span>
+                                <i class="icofont-cart cart-icon "></i>
+                                <span class="d-none d-sm-inline">السلة (<span class="cart-total"><?php echo isset($_SESSION['cart']) ? $_SESSION['cart']['totalQty'] : 0; ?></span>) منتج</span>
+                            </a>
+                        </li>
+                    </div>
+                </div>
+            </div>
+            <!-- nav -->
+        </section>
+        <!--- carousel  Start --->
--- app/views/stores/index.php
+++ app/views/stores/index.php
@@ -1,15 +1,20 @@
-<?php require APPROOT . '/app/views/inc/header.php'; ?>
-<?php flash('msg'); ?>
+<?php require APPROOT . '/app/views/inc/store-header.php';?>
+<?php flash('msg');?>
 <div class="container page">
     <div class="card" style="">
         <div class="row no-gutters">
-            <div class="col-md-5" style="background: #868e96;">
-                <img src="<?php echo (empty($data['store']->image)) ? MEDIAURL . '/default.jpg' : MEDIAURL . '/' . $data['store']->image; ?>" class="card-img-top h-100" alt="...">
+            <div class="col-12 bg-primary">
+                <h1 class="card-title text-primary text-light text-center pt-4" style="margin-right:150px !important"><?php echo $data['store']->name; ?></h1>
             </div>
-            <div class="col-md-7">
+        </div>
+        <div class="row p-3">
+            <div class="col-3" style="margin-top:-50px !important" >
+                <img src="<?php echo (empty($data['store']->employee_image)) ? MEDIAURL . '/default.jpg' : MEDIAURL . '/' . $data['store']->employee_image; ?>" class="img-thumbnail rounded-circle " alt="...">
+            </div>
+            <div class="col-9">
                 <div class="card-body">
-                    <h2 class="card-title text-primary"><?php echo $data['store']->name; ?></h2>
                     <p class="card-text"><?php echo nl2br($data['store']->details); ?></p>
+                    
                 </div>
             </div>
         </div>
@@ -25,9 +30,9 @@
         </div>
         <div class="row">
             <?php
-            echo (count($data['projects']) < 1) ? '<p class="text-center col-12 pb-5 my-5">لا يوجد منتجات تابعة لهذا المتجر </p>' : '';
-            foreach ($data['projects'] as $project) :
-            ?>
+echo (count($data['projects']) < 1) ? '<p class="text-center col-12 pb-5 my-5">لا يوجد منتجات تابعة لهذا المتجر </p>' : '';
+foreach ($data['projects'] as $project):
+?>
                 <div class="product col-12 col-xl-4 col-md-6 mt-3 wow zoomIn">
                     <form class="card" method="post" action="<?php echo URLROOT . '/carts/add/'; ?>">
                         <a href="<?php echo URLROOT . '/store/project/' . $project->project_id . '/' . $data['store']->store_id . '-' . $project->alias; ?>" class="">
@@ -38,67 +43,67 @@
                         </div>
                         <div class="p-2 amount-select">
                             <?php
-                            empty($project->fake_target) ? $target = $project->collected_traget : $target = $project->fake_target;
-                            ($project->target_price) ?: $project->target_price = 1;
-                            if ($project->enable_cart) :
-                                $donation_type = json_decode($project->donation_type);
-                            ?>
-                                <div class="my-2 btn-group-toggle" data-toggle="buttons">
-                                    <?php
-                                    switch ($donation_type->type) {
-                                        case 'share':
-                                            foreach ($donation_type->value as $value) {
-                                                echo '<label class="btn btn-secondary btn-sm m-1">
-                                                            <input type="radio" value ="' . $value->name . '" name="donation_type" required class="d-value" id="' . $value->value . '"> ' . $value->value . '
-                                                        </label>';
-                                            }
-                                            break;
-                                        case 'open':
-                                            echo '<label class="active">قم بكتابة المبلغ المراد التبرع به 
-                                                    <input type="hidden" name="donation_type" value="مفتوح"></label>';
-                                            break;
-                                        case 'unit':
-                                            foreach ($donation_type->value as $value) {
-                                                echo '<label class="btn btn-secondary btn-sm m-1">
-                                                            <input type="radio" value ="' . $value->name . '" name="donation_type" required class="d-value" id="' . $value->value . '"> ' . $value->value . '
-                                                        </label>';
-                                            }
-                                            break;
-                                        case 'fixed':
-                                            echo '<label class="btn btn-secondary btn-sm m-1">
-                                                        <input type="radio" value ="قيمة ثابته" name="donation_type" required class="d-value" id="' . $donation_type->value . '"> ' . $donation_type->value . ' ريال
-                                                    </label>';
-                                            break;
-                                    }
+                                empty($project->fake_target) ? $target = $project->collected_traget : $target = $project->fake_target;
+                                ($project->target_price) ?: $project->target_price = 1;
+                                if ($project->enable_cart):
+                                    $donation_type = json_decode($project->donation_type);
                                     ?>
-                                    <input placeholder="القيمة" min="1" type="number" class="amt col-4 rounded-lg" <?php echo ($donation_type->type == 'fixed' || $donation_type->type == 'share') ? 'readonly' : ''; ?> required name="amount">
-                                    <input type="hidden" name="project_id" value="<?php echo $project->project_id; ?>">
-                                    <input type="hidden" name="store_id" value="<?php echo $data['store']->store_id; ?>">
-                                </div>
-                            <?php endif; ?>
+                            <div class="my-2 btn-group-toggle" data-toggle="buttons">
+                                <?php
+                            switch ($donation_type->type) {
+                                case 'share':
+                                    foreach ($donation_type->value as $value) {
+                                        echo '<label class="btn btn-secondary btn-sm m-1">
+			                                                            <input type="radio" value ="' . $value->name . '" name="donation_type" required class="d-value" id="' . $value->value . '"> ' . $value->value . '
+			                                                        </label>';
+                                                        }
+                                                        break;
+                                                    case 'open':
+                                                        echo '<label class="active">قم بكتابة المبلغ المراد التبرع به
+                                                                                                            <input type="hidden" name="donation_type" value="مفتوح"></label>';
+                                                        break;
+                                                    case 'unit':
+                                                        foreach ($donation_type->value as $value) {
+                                                            echo '<label class="btn btn-secondary btn-sm m-1">
+                                                                                                                    <input type="radio" value ="' . $value->name . '" name="donation_type" required class="d-value" id="' . $value->value . '"> ' . $value->value . '
+                                                                                                                </label>';
+                                                        }
+                                                        break;
+                                                    case 'fixed':
+                                                        echo '<label class="btn btn-secondary btn-sm m-1">
+			                                                        <input type="radio" value ="قيمة ثابته" name="donation_type" required class="d-value" id="' . $donation_type->value . '"> ' . $donation_type->value . ' ريال
+			                                                    </label>';
+            break;
+    }
+    ?>
+			                                    <input placeholder="القيمة" min="1" type="number" class="amt col-4 rounded-lg" <?php echo ($donation_type->type == 'fixed' || $donation_type->type == 'share') ? 'readonly' : ''; ?> required name="amount">
+			                                    <input type="hidden" name="project_id" value="<?php echo $project->project_id; ?>">
+			                                    <input type="hidden" name="store_id" value="<?php echo $data['store']->store_id; ?>">
+			                                </div>
+			                            <?php endif;?>
                             <div class="p-2">
                                 <small class="m-0 pb-2">
                                     تم جمع
                                     <span class=" mx-1">
                                         <?php
-                                        if (!empty($project->target_unit) && !empty($project->unit_price)) { // check if user set unit and unit price
-                                            echo empty($project->fake_target) ? $target = $project->total / $project->unit_price : $target = $project->fake_target;
-                                            echo  " $project->target_unit ";
-                                        } else {
-                                            echo empty($project->fake_target) ? $target = (int) $project->total : $target = (int) $project->fake_target;
-                                            echo ' <i class="icofont-riyal"></i> ';
-                                        }
-                                        ($project->target_price) ?: $project->target_price = 1;
-                                        ?>
+if (!empty($project->target_unit) && !empty($project->unit_price)) { // check if user set unit and unit price
+    echo empty($project->fake_target) ? $target = $project->total / $project->unit_price : $target = $project->fake_target;
+    echo " $project->target_unit ";
+} else {
+    echo empty($project->fake_target) ? $target = (int) $project->total : $target = (int) $project->fake_target;
+    echo ' <i class="icofont-riyal"></i> ';
+}
+($project->target_price) ?: $project->target_price = 1;
+?>
                                     </span>
                                 </small>
                                 <small class=" pt-1 float-left"><span>المستهدف : </span>
                                     <span><?php echo $project->target_price; ?></span>
                                     <?php if (empty($project->target_unit)) {
-                                        echo '<i class="icofont-riyal"></i>';
-                                    } else {
-                                        echo  $project->target_unit;
-                                    }  ?>
+    echo '<i class="icofont-riyal"></i>';
+} else {
+    echo $project->target_unit;
+}?>
                                 </small>
                                 <div class="progress my-1">
                                     <h6 class="p-1 progress-bar progress-bar-striped bg-success" role="progressbar" style="width:<?php echo ceil($target * 100 / $project->target_price) . "%"; ?>" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
@@ -112,16 +117,16 @@
                                 <a href="<?php echo URLROOT . '/store/project/' . $project->project_id . '/' . $data['store']->store_id . '-' . $project->alias; ?>" class="card-text">
                                     <i class="icofont-files-stack"></i> التفاصيل
                                 </a>
-                                <?php if ($project->enable_cart) : ?>
-                                    <button class="cart-add" name="projectCategories" value="<?php echo  $project->category_id; ?>" type="submit"><i class="icofont-cart-alt"></i> اضف الي السلة</button>
+                                <?php if ($project->enable_cart): ?>
+                                    <button class="cart-add" name="projectCategories" value="<?php echo $project->category_id; ?>" type="submit"><i class="icofont-cart-alt"></i> اضف الي السلة</button>
                                     <input type="number" name="quantity" min="1" value="1" required id="quantity" class="col-2 py-0 px-0 float-left">
                                     <input type="hidden" name="url" value="<?php echo URLROOT . '/carts/ajaxAdd'; ?>">
-                                <?php endif; ?>
+                                <?php endif;?>
                             </div>
                         </div>
                     </form>
                 </div> <!-- end product -->
-            <?php endforeach; ?>
+            <?php endforeach;?>
 
         </div>
         <div class="row ">
@@ -140,7 +145,7 @@
                 <div class="modal-body pt-0">
                 </div>
                 <div class="p-2 border-top">
-                    <a href="<?php root('carts') ?>" class="btn btn-primary">عرض السلة</a>
+                    <a href="<?php root('carts')?>" class="btn btn-primary">عرض السلة</a>
                     <button type="button" class="btn btn-danger float-left" data-dismiss="modal">اغلاق</button>
                 </div>
             </div>
@@ -148,9 +153,9 @@
     </div>
     <!-- end products -->
 </div>
-<?php require APPROOT . '/app/views/inc/footer.php'; ?>
+<?php require APPROOT . '/app/views/inc/footer.php';?>
 <script>
-    //submitting amount value 
+    //submitting amount value
     // if user select from units or fixed or share donation
     $('.d-value').change(function() {
         var amount = $(this).attr('id');
